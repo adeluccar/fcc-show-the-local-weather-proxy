b291493d1faf5a8ceccb4b3b45d1609db0f81a50
Alberto De Lucca <albertodelucca@gmail.com>
2017-07-27 23:46:44 -0400

    Config the port according to Heroku
    https://stackoverflow.com/a/13013189/1253966

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('/', function(req, res){
   }).end();
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log('Example app listening on port 3000!')
 })
